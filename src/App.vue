adce2f913297cf64b06272cb81f6b1066c14a6df
Ali Sattar <alirsattar@gmail.com>
2020-03-14 13:52:45 -0400

    Setting up initial file structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <ListView />
+
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ListView from "./components/ListView";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListView
+    // HelloWorld
   }
 }
 </script>
